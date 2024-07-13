12371c465be93a3594d82fb1583c4708fdbc666a
Aasim_Sayyed <126641322+aasim-s@users.noreply.github.com>
2024-07-13 16:14:57 +0530

    Create Matrix_times_Vector.py

---
+++ Matrix_times_Vector.py
@@ -0,0 +1,24 @@
+def matrix_dot_vector(a: list[list[int | float]], b: list[int | float]) -> list[int | float]:
+    vector_len = len(b)
+    matrix_rows = len(a)
+    c = []
+
+    if len(a[0]) != vector_len:
+        return [-1]
+
+    for i in range(matrix_rows):
+        total = 0
+        for j in range(vector_len):
+            total += a[i][j] * b[j]
+        c.append(total)
+
+    return c
+
+
+print(matrix_dot_vector([[1, 2, 3], [2, 4, 5], [6, 8, 9]], [1, 2, 3]))  #[14, 25, 49]
+
+"""
+[1,2,3]  [1,2,3]   1+4+9   = 14
+[2,4,5]            2+8+15  = 25
+[6,8,9]            6+16+27 = 49 
+"""
